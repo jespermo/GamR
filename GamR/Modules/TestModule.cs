7ce67147c61c57ef9e8caad4c58c64be46a54fa8
Jesper Møjbæk <jmo@d60.dk>
2017-06-07 22:11:29 +0200

    Initial project

---
+++ GamR/Modules/TestModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace GamR.Modules
+{
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get("/", args => "Hej dut");
+        }
+    }
+}
