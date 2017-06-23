0e1fab8bed876a33403a99af0af0da71e4fe3136
Jesper Møjbæk <jmo@d60.dk>
2017-06-23 19:15:47 +0200

    Added core project

---
+++ GamR.Backend.Core/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GamR.Backend.Core
+{
+    public class PlayerAggregate
+    {
+        public string Name { get; }
+
+        private PlayerAggregate(string name)
+        {
+            Name = name;
+        }
+
+        static PlayerAggregate Create(string name)
+        {
+            return new PlayerAggregate(name);
+        }
+    }
+}
