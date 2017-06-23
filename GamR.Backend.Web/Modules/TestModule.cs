6ced80c8b17ca4cea10a7c9441e592e6e1ec70ac
jbm <jbm@d60.dk>
2017-06-23 19:44:46 +0200

    clean up

--- GamR.Backend.Web/Modules/TestModule.cs
+++ GamR.Backend.Web/Modules/TestModule.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Nancy;
+﻿using Nancy;
 
-namespace GamR.Modules
+namespace GamR.Backend.Web.Modules
 {
     public class TestModule : NancyModule
     {
